1a8202eeed28238634475b0637f3763ad909f67b
Aaron Austin <aaron@aaronaustin.com>
2021-10-27 14:38:01 -0400

    changed back link

--- src/components/layout.js
+++ src/components/layout.js
@@ -15,6 +15,10 @@ const Layout = ({ location, title, children }) => {
   } else {
     header = (
       <Link className='header-link-home' to='/'>
+        <svg height='24px' viewBox='0 0 24 24' width='24px' fill='currentColor'>
+          <path d='M0 0h24v24H0z' fill='none' />
+          <path d='M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z' />
+        </svg>
         {title}
       </Link>
     );
