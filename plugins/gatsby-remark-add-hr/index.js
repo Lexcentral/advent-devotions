bff942b5ad958f8a9870b0752667a5de298ef1db
Aaron Austin <aaron@aaronaustin.com>
2023-10-30 11:53:01 -0400

    update unist-util-visit package.json to ^2

--- plugins/gatsby-remark-add-hr/index.js
+++ plugins/gatsby-remark-add-hr/index.js
@@ -1,17 +1,17 @@
 const visit = require('unist-util-visit'); //version 2.0.3 required to work until Gatby allows import here.
-
+// import * as visit from 'unist-util-visit';
 module.exports = ({ markdownAST }) => {
-  visit(markdownAST, 'text', (node) => {
-    // let { depth } = node;
-    // // Skip if not an h1
-    // if (depth !== 1) return;
-    // Grab the innerText of the heading node
-    // let text = toString(node);
-    // let html = node.value.replace(/\u000b/g, '<br/>');
-    let html = node.value.replace(/\*\*\*/g, '<hr/>');
-    node.type = 'html';
-    node.children = undefined;
-    node.value = html;
-  });
-  return markdownAST;
+	visit(markdownAST, 'text', (node) => {
+		// let { depth } = node;
+		// // Skip if not an h1
+		// if (depth !== 1) return;
+		// Grab the innerText of the heading node
+		// let text = toString(node);
+		// let html = node.value.replace(/\u000b/g, '<br/>');
+		let html = node.value.replace(/\*\*\*/g, '<hr/>');
+		node.type = 'html';
+		node.children = undefined;
+		node.value = html;
+	});
+	return markdownAST;
 };
